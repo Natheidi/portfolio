bb9782c4077db214ec87b20e1abac033f2cc6fb9
Grygier <kwaska1000@gmail.com>
2022-04-01 17:45:46 +0200

    js animacje

---
+++ Natalia/js/nairo.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibmF0aGVpZGkiLCJhIjoiY2wxZXlmd21nMDBuZDNjcGJ3d2l6eWd0NyJ9.W2_Se-1jatLqHXB14r6PRg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
