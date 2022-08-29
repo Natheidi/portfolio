f1f0661d4cbd21e58854c1abbfca030ec18ebdc1
Natalia <nathbyrdy@gmail.com>
2022-08-29 13:05:53 +0200

    Rename files

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,11 +5,7 @@ function toggleMenu() {
     const menuList = document.querySelector('.side-nav-hamburger'); 
     if (menuList.classList.contains("visible")) {
         menuList.classList.remove("visible");
-        // closeIcon.style.display = "none";
-        // menuIcon.style.display = "block";
     } else {
         menuList.classList.add("visible");
-        // closeIcon.style.display = "block";
-        // menuIcon.style.display = "none";
     }
 }
