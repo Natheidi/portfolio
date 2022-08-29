7ae42bc6a253cefc60573033354dec542fca25b4
Natalia <nathbyrdy@gmail.com>
2022-08-29 12:55:56 +0200

    Change root

---
+++ js/nairo-hamburger.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    const menuList = document.querySelector('.side-nav-hamburger'); 
+    if (menuList.classList.contains("visible")) {
+        menuList.classList.remove("visible");
+        // closeIcon.style.display = "none";
+        // menuIcon.style.display = "block";
+    } else {
+        menuList.classList.add("visible");
+        // closeIcon.style.display = "block";
+        // menuIcon.style.display = "none";
+    }
+}
